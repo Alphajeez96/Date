fcfb180fefbb2793f8370a7f2e9c50bff624fc7b
Prince <chimeremezechukwudire@gmail.com>
2020-08-11 00:22:30 +0100

    modal script optimized

--- Js/index.js
+++ Js/index.js
@@ -8,10 +8,17 @@ toggle.addEventListener('click', function(e) {
 });
 
 
+//Modal togglers here
 let modal_toggle = document.querySelector('#modaltoggle');
 let modal = document.querySelector('#modal').style.display = 'none';
+let modal_close = document.querySelector('#modalclose');
 
 modal_toggle.addEventListener('click', function(e) {
     e.preventDefault();
     modal = document.querySelector('#modal').style.display = 'flex';
+})
+
+modal_close.addEventListener('click', function(e) {
+    e.preventDefault(e)
+    modal = document.querySelector('#modal').style.display = 'none';
 })
