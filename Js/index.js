2b4472dc283618eb64c64fcced242a6ecf904486
Prince <chimeremezechukwudire@gmail.com>
2020-08-11 00:07:24 +0100

    modal script added

--- Js/index.js
+++ Js/index.js
@@ -8,23 +8,10 @@ toggle.addEventListener('click', function(e) {
 });
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
+let modal_toggle = document.querySelector('#modaltoggle');
+let modal = document.querySelector('#modal').style.display = 'none';
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = 'https://cdn.pixabay.com/photo/2020/07/02/17/29/landscape-5363681_960_720.jpg';
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+modal_toggle.addEventListener('click', function(e) {
+    e.preventDefault();
+    modal = document.querySelector('#modal').style.display = 'flex';
+})
