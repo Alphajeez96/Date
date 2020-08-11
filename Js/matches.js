87cb1a39748761ce8306689505d5a4ed7f4b094e
Prince <chimeremezechukwudire@gmail.com>
2020-08-11 19:19:01 +0100

    scripts added

---
+++ Js/matches.js
@@ -0,0 +1,24 @@
+//Dark mode toggler
+function addDarkmodeWidget() {
+    new Darkmode().showWidget();
+}
+window.addEventListener('load', addDarkmodeWidget);
+
+// Chat Viewer here
+
+function onTabClick(event) {
+    let activeTabs = document.querySelectorAll('.active');
+
+    // deactivate existing active tab and panel 
+    activeTabs.forEach(function(tab) {
+        tab.className = tab.className.replace('active', '');
+    });
+
+    // activate new tab and panel
+    event.target.parentElement.className += ' active';
+    document.getElementById(event.target.href.split('#')[1]).className += ' active';
+}
+
+const element = document.getElementById('nav-tab');
+
+element.addEventListener('click', onTabClick, false);
