43d4397343e9a8494e31f3691ffd1f6208432239
Prince <chimeremezechukwudire@gmail.com>
2020-08-13 20:23:19 +0100

    chat setup loop test

--- Js/matches.js
+++ Js/matches.js
@@ -31,14 +31,16 @@ const suggestions = [{
         address: "24, Arizona",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At vitae, at scelerisque scelerisque morbi.",
         src: './Assets/Vectors/Ellipse 23.jpg',
-        id: 1
+        id: 1,
+        chat: 'I have heard 1'
     },
     {
         name: "Brianna Doe",
         address: "24, Arizona",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At vitae, at scelerisque scelerisque morbi.",
         src: './Assets/Vectors/Ellipse 22.png',
-        id: 2
+        id: 2,
+        chat: 'I have heard 2'
 
     },
     {
@@ -46,59 +48,67 @@ const suggestions = [{
         address: "24, Arizona",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At vitae, at scelerisque scelerisque morbi.",
         src: './Assets/Vectors/Ellipse 25.png',
-        id: 3
+        id: 3,
+        chat: 'I have heard 3'
     },
     {
         name: "John Doe",
         address: "24, Arizona",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At vitae, at scelerisque scelerisque morbi.",
         src: './Assets/Vectors/Ellipse 23.jpg',
-        id: 4
+        id: 4,
+        chat: 'I have heard 4'
     }
 ];
 
-document.querySelector('.contents')
-    .innerHTML = suggestions.reduce((a, sg) => a +=
-        `<div class="container justify-end flex">
+//Looping per object
+// document.querySelector('.contents')
+//     .innerHTML = suggestions.reduce((a, sg) => a +=
+//         `<div class="container justify-end flex">
+//         <div class="  w-3/4 mx-4 bg-white shadow-lg rounded-lg overflow-hidden">
+//         <div class="sm:flex sm:items-center px-6 py-5">
+//             <img class="block mx-auto sm:mx-0 sm:flex-shrink-0 rounded-full" src='${sg.src}' alt="Woman's Face">
+//             <div class="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
+//                 <p class="text-xl  leading-tight">${sg.name}</p>
+//                 <p class="text-sm pt-2 leading-tight text-gray-600">${sg.address}</p>
+//                 <p class="text-sm leading-tight pt-2 ">${sg.description}</p>
+//             </div>
+//             <div class="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
+//             </div>
+//         </div>
+//      </div>
+// </div>`, '');
+
+const chats = [];
+
+const mountchats = () => {
+    let menu = document.querySelector('.contents')
+    suggestions.forEach(e => {
+        const me = document.createElement('div')
+        me.classList.add('order2')
+        me.innerHTML = `<div class="container justify-end flex">
         <div class="  w-3/4 mx-4 bg-white shadow-lg rounded-lg overflow-hidden">
         <div class="sm:flex sm:items-center px-6 py-5">
-            <img class="block mx-auto sm:mx-0 sm:flex-shrink-0 rounded-full" src='${sg.src}' alt="Woman's Face">
+            <img class="block mx-auto sm:mx-0 sm:flex-shrink-0 rounded-full" src='${e.src}' alt="Woman's Face">
             <div class="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
-                <p class="text-xl  leading-tight">${sg.name}</p>
-                <p class="text-sm pt-2 leading-tight text-gray-600">${sg.address}</p>
-                <p class="text-sm leading-tight pt-2 ">${sg.description}</p>
+                <p class="text-xl  leading-tight">${e.name}</p>
+                <p class="text-sm pt-2 leading-tight text-gray-600">${e.address}</p>
+                <p class="text-sm leading-tight pt-2 ">${e.description}</p>
             </div>
             <div class="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
             </div>
         </div>
      </div>
-</div>`, '');
-
-const images = [];
-
-const mountImages = () => {
-    let menu = document.querySelector('.menu')
-    suggestions.forEach(e => {
-        const me = document.createElement('div')
-        me.classList.add('order2')
-        me.innerHTML = `
-        <div>
-        <img src='${e.src}'>
-        </div>
-        <div class='choose'>
-        <h3>${e.name}</h3>
-        <h3>${e.address}</h3>
-        </div>
-        `
+</div>`
         menu.appendChild(me);
 
-        images.push(me)
+        chats.push(me)
     })
-    console.log(images)
+    console.log(chats)
 }
-mountImages();
+mountchats();
 
-images.forEach((e, i) => e.addEventListener('click', a => {
+chats.forEach((e, i) => e.addEventListener('click', a => {
     let select = document.querySelector('#chathistory')
     const chatter = document.createElement('div')
     chatter.innerHTML = ` <div>
